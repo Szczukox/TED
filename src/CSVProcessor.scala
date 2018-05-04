da33ab6a895ca760cbeb85b4fa952e4690ef65a8
Szczukox <33254494+Szczukox@users.noreply.github.com>
2018-05-04 19:59:28 +0200

    Add feature mean per year of country contract value

--- src/CSVProcessor.scala
+++ src/CSVProcessor.scala
@@ -1,13 +1,34 @@
 class CSVProcessor(csvFile: String) {
   import io.Source
   import scala.collection.mutable.Map
-  val source = Source.fromURL(csvFile)
-  val countryID = source.getLines().next().split(",(?!(?=[^\"]*\"[^\"]*(?:\"[^\"]*\"[^\"]*)*$))").map(_.trim).indexOf("\"ISO_COUNTRY_CODE\"")
-  var countryIdMap = Map[String, Int]()
+  import java.util.Locale
+  val source = Source.fromFile(csvFile)
+  val head = source.getLines().next().split(",(?!(?=[^\"]*\"[^\"]*(?:\"[^\"]*\"[^\"]*)*$))")
+  val isoID = head.indexOf("\"ISO_COUNTRY_CODE\"")
+  val valueID = head.indexOf("\"VALUE_EURO_FIN_1\"")
+  var isoMap = Map[String, Int]()
+  var valueMap = Map[String, Double]()
   for (row <- source.getLines()) {
-    val columns = row.split(",(?!(?=[^\"]*\"[^\"]*(?:\"[^\"]*\"[^\"]*)*$))").map(_.trim)
-    if (countryIdMap.contains(columns(countryID))) countryIdMap(columns(countryID)) = countryIdMap(columns(countryID)) + 1
-    else countryIdMap += (columns(countryID) -> 1)
+    val columns = row.split(",(?!(?=[^\"]*\"[^\"]*(?:\"[^\"]*\"[^\"]*)*$))")
+    if (columns.length > valueID) {
+      if (columns(valueID) != "") {
+        val countryName = new Locale("", columns(isoID).substring(1, columns(isoID).length - 1)).getDisplayCountry
+        if (isoMap.contains(countryName)) {
+          isoMap(countryName) = isoMap(countryName) + 1
+          valueMap(countryName) += columns(valueID).toDouble
+        }
+        else{
+          isoMap += (countryName -> 1)
+          valueMap += (countryName -> columns(valueID).toDouble)
+        }
+      }
+    }
   }
-  println(countryIdMap)
+  println(isoMap)
+  println(isoMap.values.sum)
+  println("")
+  for (key <- isoMap.keys) {
+    valueMap(key) /= isoMap(key)
+  }
+  println(valueMap)
 }
--- src/Run.scala
+++ src/Run.scala
@@ -1,3 +1,3 @@
 object Run extends App {
-  new CSVProcessor("http://data.europa.eu/euodp/repository/ec/dg-grow/mapps/TED_CAN_2006.csv")
+  new CSVProcessor("TED_CAN_2006.csv")
 }
