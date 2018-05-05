10ee931b0d31148cf2f1b7c4fd9d861b3c305f6a
Szczukox <33254494+Szczukox@users.noreply.github.com>
2018-05-05 19:53:12 +0200

    Add final function

--- src/CSVProcessor.scala
+++ src/CSVProcessor.scala
@@ -1,34 +1,28 @@
-class CSVProcessor(csvFile: String) {
-  import io.Source
-  import scala.collection.mutable.Map
-  import java.util.Locale
-  val source = Source.fromFile(csvFile)
-  val head = source.getLines().next().split(",(?!(?=[^\"]*\"[^\"]*(?:\"[^\"]*\"[^\"]*)*$))")
-  val isoID = head.indexOf("\"ISO_COUNTRY_CODE\"")
-  val valueID = head.indexOf("\"VALUE_EURO_FIN_1\"")
-  var isoMap = Map[String, Int]()
-  var valueMap = Map[String, Double]()
-  for (row <- source.getLines()) {
-    val columns = row.split(",(?!(?=[^\"]*\"[^\"]*(?:\"[^\"]*\"[^\"]*)*$))")
-    if (columns.length > valueID) {
-      if (columns(valueID) != "") {
-        val countryName = new Locale("", columns(isoID).substring(1, columns(isoID).length - 1)).getDisplayCountry
-        if (isoMap.contains(countryName)) {
-          isoMap(countryName) = isoMap(countryName) + 1
-          valueMap(countryName) += columns(valueID).toDouble
-        }
-        else{
-          isoMap += (countryName -> 1)
-          valueMap += (countryName -> columns(valueID).toDouble)
+import scala.collection.mutable.Map
+class CSVProcessor(csvFile: String, isoMap: Map[String, Int], valueMap: Map[String, Double]) {
+  def process() : (Map[String, Int], Map[String, Double]) = {
+    import io.Source
+    import java.util.Locale
+    val source = Source.fromFile(csvFile)
+    val head = source.getLines().next().split(",(?!(?=[^\"]*\"[^\"]*(?:\"[^\"]*\"[^\"]*)*$))")
+    val isoID = head.indexOf("\"ISO_COUNTRY_CODE\"")
+    val valueID = head.indexOf("\"VALUE_EURO_FIN_1\"")
+    for (row <- source.getLines()) {
+      val columns = row.split(",(?!(?=[^\"]*\"[^\"]*(?:\"[^\"]*\"[^\"]*)*$))")
+      if (columns.length > valueID) {
+        if (columns(valueID) != "") {
+          val countryName = new Locale("", columns(isoID).substring(1, columns(isoID).length - 1)).getDisplayCountry
+          if (isoMap.contains(countryName)) {
+            isoMap(countryName) = isoMap(countryName) + 1
+            valueMap(countryName) += columns(valueID).toDouble
+          }
+          else {
+            isoMap += (countryName -> 1)
+            valueMap += (countryName -> columns(valueID).toDouble)
+          }
         }
       }
     }
+    return (isoMap, valueMap)
   }
-  println(isoMap)
-  println(isoMap.values.sum)
-  println("")
-  for (key <- isoMap.keys) {
-    valueMap(key) /= isoMap(key)
-  }
-  println(valueMap)
 }
--- src/Run.scala
+++ src/Run.scala
@@ -1,3 +1,19 @@
 object Run extends App {
-  new CSVProcessor("TED_CAN_2006.csv")
+  import scala.collection.mutable.Map
+  import java.text.DecimalFormat
+  val dateList = Array("TED_CAN_2006.csv", "TED_CAN_2007.csv")
+  var contractSumAmount = Map[String, Int]()
+  var contractSumValue = Map[String, Double]()
+  for (file <- dateList) {
+    val csvFile = new CSVProcessor(file, contractSumAmount, contractSumValue)
+    val dataTuple = csvFile.process()
+    contractSumAmount = dataTuple._1
+    contractSumValue = dataTuple._2
+  }
+  for (key <- contractSumAmount.keys) {
+    println("Średnia liczba zamówień dla kraju " + key + " wynosi: " + contractSumAmount(key) / dateList.length)
+    val decimalFormat = new DecimalFormat("0,000.00")
+    println("Średnia kwota zamówienia dla kraju " + key + " wynosi: " + decimalFormat.format(contractSumValue(key) / contractSumAmount(key)) + " EURO")
+    println("")
+  }
 }
